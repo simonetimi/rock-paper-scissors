4a7c1ecbedc98b53c291c267b689f73c6bc2f2d5
Simone <150595274+amnotsimon@users.noreply.github.com>
2023-11-18 22:36:20 +0100

    Add score variables

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let computerSelection
 let playerSelection
+let computerScore
+let playerScore
 let roundResult
 
 function getComputerChoice() {
@@ -17,29 +19,42 @@ function getComputerChoice() {
 function getPlayerChoice() {
     playerInput = prompt('Rock, Paper or Scissors?', '');
     playerSelection = playerInput[0].toUpperCase() + playerInput.slice(1);
-    return playerSelection
+    return playerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'Rock' && computerSelection == 'Rock') {
         roundResult = 'It\'s a tie! You both picked Rock'
+        computerScore++;
+        playerScore++;
     } else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
         roundResult = 'It\'s a tie! You both picked Paper'
+        computerScore++;
+        playerScore++;
     } else if (playerSelection == 'Scissors' && computerSelection == 'Scissors') {
         roundResult = 'It\'s a tie! You both picked Scissors'
+        computerScore++;
+        playerScore++;
     } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
         roundResult = 'You lose! Paper beats Rock'
+        computerScore++;
     } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
         roundResult = 'You lose! Scissors beats Paper'
+        computerScore++;
     } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
         roundResult = 'You lose! Rock beats Scissors'
+        computerScore++;
     } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
         roundResult = 'You win! Rock beats Scissors'
+        playerScore++;
     } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
         roundResult = 'You win! Scissors beats Paper'
+        playerScore++;
     } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
         roundResult = 'You win! Paper beats Rock'
+        playerScore++;
     }
-    return roundResult;
+    alert(roundResult);
 }
  
+
