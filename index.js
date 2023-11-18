7aad851491fa62c17a5a62f666d636b57dea512a
Simone <150595274+amnotsimon@users.noreply.github.com>
2023-11-18 23:33:38 +0100

    Improve efficiency and add game function
    Made the play round function less redudant and added the final game function for playing 5 rounds in a row. It will return the final score.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let computerSelection
 let playerSelection
-let computerScore
-let playerScore
-let roundResult
+let computerScore = 0
+let playerScore = 0
+let roundResult = ''
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
@@ -22,39 +22,44 @@ function getPlayerChoice() {
     return playerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'Rock' && computerSelection == 'Rock') {
-        roundResult = 'It\'s a tie! You both picked Rock'
+function playRound() {
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+    if ((playerSelection == 'Rock' && computerSelection == 'Rock') ||
+        (playerSelection == 'Paper' && computerSelection == 'Paper') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Scissors')) {
+        roundResult = `It's a tie! You both picked ${playerSelection}`
         computerScore++;
         playerScore++;
-    } else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
-        roundResult = 'It\'s a tie! You both picked Paper'
+    } else if ((playerSelection == 'Rock' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Rock')) {
+        roundResult = `You lose! ${computerSelection} beats ${playerSelection}`
         computerScore++;
-        playerScore++;
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Scissors') {
-        roundResult = 'It\'s a tie! You both picked Scissors'
-        computerScore++;
-        playerScore++;
-    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        roundResult = 'You lose! Paper beats Rock'
-        computerScore++;
-    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        roundResult = 'You lose! Scissors beats Paper'
-        computerScore++;
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-        roundResult = 'You lose! Rock beats Scissors'
-        computerScore++;
-    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        roundResult = 'You win! Rock beats Scissors'
-        playerScore++;
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        roundResult = 'You win! Scissors beats Paper'
-        playerScore++;
-    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-        roundResult = 'You win! Paper beats Rock'
+    } else if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock')) {
+        roundResult = `You win! ${playerSelection} beats ${computerSelection}`
         playerScore++;
     }
+    console.log(roundResult)
     alert(roundResult);
+    return roundResult;
 }
  
+function game() {
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    if (playerScore == computerScore) {
+        alert('Final result: Wow, it\'s a tie!')
+    } else if (playerScore > computerScore)  {
+        alert('Final result: Congratulations, you won!') 
+    } else {
+        alert('Final Result: You lost, but you can try again!') 
+    }
+}
 
+game()
